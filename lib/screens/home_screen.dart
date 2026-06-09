e564c70cfae12b456e618c278c32de8834f00ba1
novalarama <novalakbarramadhany@gmail.com>
2026-06-09 15:40:04 +0700

    feat/test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -236,7 +236,7 @@ class _HomeScreenState extends State<HomeScreen> {
                               });
                             },
                             decoration: InputDecoration(
-                              hintText: 'Search New Task Here',
+                              hintText: 'Search Task',
                               hintStyle: AppTextStyles.bodyMedium.copyWith(
                                   color: Theme.of(context)
                                       .colorScheme
